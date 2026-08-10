cae4d03aa0b91290132b5792cde02092350ee305
Md.Srabon <mdsrabon.cse@gmail.com>
2026-08-11 02:04:49 +0600

    Create FounderController.cs

---
+++ RiseUp Community/RiseUp Community/Controllers/FounderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RiseUp.Web.Controllers
+{
+    [Authorize(Roles = "Founder")]
+    public class FounderController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
