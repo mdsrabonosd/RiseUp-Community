bb1b8d23648ab5c68785b70bbbad1518af9f0d91
Md.Srabon <mdsrabon.cse@gmail.com>
2026-08-11 23:44:05 +0600

    Create MentorController.cs

---
+++ RiseUp Community/RiseUp Community/Controllers/MentorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RiseUp.Web.Controllers
+{
+    [Authorize(Roles = "Mentor")]
+    public class MentorController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
