e8fa3711084a2b2e89bcbb9aa26ee30686c5dd74
Md.Srabon <mdsrabon.cse@gmail.com>
2026-08-11 23:44:37 +0600

    Create InvestorController.cs

---
+++ RiseUp Community/RiseUp Community/Controllers/InvestorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RiseUp.Web.Controllers
+{
+    [Authorize(Roles = "Investor")]
+    public class InvestorController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
