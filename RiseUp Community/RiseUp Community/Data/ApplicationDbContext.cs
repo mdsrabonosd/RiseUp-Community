bbe359c5dd82c24ed31f372b83b7ba46073d041c
Md.Srabon <mdsrabon.cse@gmail.com>
2026-08-12 00:35:30 +0600

    Update ApplicationDbContext.cs

--- RiseUp Community/RiseUp Community/Data/ApplicationDbContext.cs
+++ RiseUp Community/RiseUp Community/Data/ApplicationDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace RiseUp_Community.Data
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RiseUp_Community.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
     }
 }
