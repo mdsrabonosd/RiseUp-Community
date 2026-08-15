40ac8365b044df3ea99d933493cdfc20668c2b4b
Md.Srabon <mdsrabon.cse@gmail.com>
2026-08-16 00:18:40 +0600

    Create ApplicationUser.cs

---
+++ RiseUp Community/RiseUp Community/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RiseUp_Community.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string UserRole { get; set; } = string.Empty; // "Startup", "Investor", "Admin"
+        public string? Role { get; set; } // <-- এই প্রপার্টিটি মিসিং ছিল
+    }
+}
